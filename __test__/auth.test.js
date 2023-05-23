29e573e52854bc0a248abf8615439dd22e90e0dd
KaedenOC <komeacarr2@gmail.com>
2023-05-23 13:28:54 -0700

    tests

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -1,39 +1,5 @@
 'use strict';
 
-
-// const supertest = require('supertest');
-// const { app } = require('../src/server');
-// const { sequelize } = require('../src/auth/models');
-
-// const request = supertest(app);
-
-// beforeAll(async () => {
-//   await sequelize.sync();
-// });
-
-// afterAll( async () => {
-//   await sequelize.drop();
-// });
-
-// describe( 'Auth Routes', (() => {
-
-//   test('Allow for user signup', async () => {
-//     const res = await request.post('/signup').send({
-//       username: 'enzo',
-//       password: 'woof',
-//     });
-//     expect(res.status).toEqual(201);
-//     expect(res.body.username).toEqual( 'enzo');
-//   });
-
-
-
-// }));
-
-
-
-
-
 const supertest = require('supertest');
 const { sequelize } = require('../src/auth/models');
 const { app } = require('../src/server');
